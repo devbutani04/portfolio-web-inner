75e1a18940d2c22fc62ea74b76bdfeaaf3a25ea4
devbutani04 <devbutani2462@gmail.com>
2024-07-04 17:51:17 +0530

    files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
 import Desktop from './components/os/Desktop';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReportBugButton from './other/ReportBugButton';
 
 function App() {
     return (
         <div className="App">
             <Desktop />
+            <ReportBugButton />
         </div>
     );
 }
