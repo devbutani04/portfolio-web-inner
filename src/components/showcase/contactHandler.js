f76a4b3eb4670d07f9768e01ea1204fd06d7d81c
devbutani04 <devbutani2462@gmail.com>
2024-07-05 11:13:52 +0530

    contact

--- src/components/showcase/contactHandler.js
+++ src/components/showcase/contactHandler.js
@@ -45,10 +45,10 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// Get the button, and when the user clicks on it, execute myFunction
-document.querySelector("myBtn").onclick = function() {myFunction()};
+// // Get the button, and when the user clicks on it, execute myFunction
+// document.querySelector("myBtn").onclick = function() {myFunction()};
 
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-function myFunction() {
-  document.querySelector("myDropdown").classList.toggle("show");
-}
+// /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+// function myFunction() {
+//   document.querySelector("myDropdown").classList.toggle("show");
+// }
