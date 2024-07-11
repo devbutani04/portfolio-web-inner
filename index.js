41fd4d42220521945f8eb2b51a9e1cdcdc48aff5
devbutani04 <devbutani2462@gmail.com>
2024-07-11 13:17:34 +0530

    js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
