da7853987ca86e43e4e5584dedef2957e6fbc2d6
devbutani04 <168613965+devbutani04@users.noreply.github.com>
2025-03-03 10:59:36 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+// externals: {
+//     'Config': JSON.stringify(process.env.NODE_ENV === 'production' ? {
+//       serverUrl: "https://www.quillix.in"
+//     } : {
+//       serverUrl: "http://localhost:5000"
+//     })
+//   }
